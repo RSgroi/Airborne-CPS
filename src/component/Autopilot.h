6a07405208313bbf2ef51f4533b26cbf6cbe4083
Andrew Driscoll <adriscol@oswego.edu>
2019-03-11 06:50:06 -0400

    Added AutoPilot Classes

---
+++ src/component/Autopilot.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "XPLMUtilities.h"
+
+class Autopilot {
+public:
+
+protected:
+
+private:
+
+	static void recommendLift() {
+		// sim/autopilot/altitude_up 
+	}
+
+	static void recommendDrop() {
+		// sim/autopilot/altitude_down
+	}
+
+	static void changeHeaderLeft() {
+		// sim/autopilot/heading_copilot_down
+	}
+
+	static void changeHeaderRight() {
+		// sim/autopilot/heading_copilot_up
+	}
+};
